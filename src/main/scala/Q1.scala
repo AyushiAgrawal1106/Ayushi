04397d8ee6ed969bf32016f36e59b431fa01fb92
hj886 <ayushiagrwl11@gmail.com>
2024-01-26 19:29:29 +0530

    Created program for converting kg to gm

---
+++ src/main/scala/Q1.scala
@@ -0,0 +1,9 @@
+object Q1 {
+  def main(args: Array[String]): Unit ={
+    var a:Int=56
+    var b:Int = a * 1000
+
+    print(b+" gm")
+
+  }
+}
---
+++ src/main/scala/second.scala
@@ -0,0 +1,8 @@
+object second {
+  def main(args: Array[String]): Unit = {
+    val a = 20
+    val b: Int = 80
+    println(a)
+    println(b)
+  }
+}
